6db6131c667fc6d7bc22c12a73874e12e18348cb
somersgreg <somersgreg@yahoo.com>
2021-11-06 00:21:33 -0400

    > Notes add for half of review.py

--- review.py
+++ review.py
@@ -1,16 +1,21 @@
 import random
 import time
 
-
+# Classes are always upper Camel-case with first letter capped. DF, Series are also classes
 class BaseCharacter:
+    # First first always write doc string.
+    # This says the parameter is 'name', its a string, and what it 'is'.
     """
     This is a base character model for our game.
 
     :param name: str - Name of the character
     """
-
+    # Every class needs a constructor, dunder init, is first thing you need.
+    # 'Self' is there so everything can be 'aware of everything else - worth researching.
     def __init__(self, name):
+        # In the constructor using reflection (self) we instantiate the 'name' attribute.
         self.name = name
+        # . . . as well as these attributes.
         self.level = 1
 
         random_stat = self._rollD20()
@@ -46,24 +51,44 @@ class BaseCharacter:
         print(f'Rolled a {die_value}!')
 
         return die_value
-
+    # (self, identifier = random.randint) to get a rnd default value.
+    # Import random for this or `from random import randint`
+    # generate an integer between 3 and 9 (inclusive range).
+    # (see randrange for exclusive range)
+    # print(randint(3, 9))
     def level_up(self, increment=1):
         """
         Levels up a character by an increment
 
         :param increment: int (default=1) - the number of levels to level up a character
         """
+        # Exceptions: rnd must be greater or equal to 1
         if increment < 1:
             raise Exception('Parameter "increment" must be an integer greater than or equal to 1.')
 
+        # We're getting a python dictionary of all the keys and values (self.name,self.int,etc) associated with self.
+        # This is above stat_increase so it includes stat_increase.
+        # dict_items = {'a':3, 'b':2, 'c':1} then
+        # >>> dict_items.keys() gives dict_keys(['a', 'b', 'c'])
+        # >>> dict_items.values() gives dict_values([3, 2, 1])
+        # >>> dict_items.items() gives dict_keys([('a',3), ('b',2), ('c',1)])
+        # which we need as its a iterable list of two variables, (k, v).
+        # Also, if we enter `dict_items['b'] += 1` then b will = 3.
         dict_items = vars(self)
+        # Each level stats increase 3 times the increment #
         stat_increase = 3 * increment
-
+        # We create a for loop that will iterate the class attributes and increment the main stats (int,str,agi,...)
+        # by stat_increase and the level, by the value of increment.
         for k, v in list(dict_items.items()):
+            # if 'v'(value) is an int and 'k'(key) doesn't equal current level:
             if type(v) == int and k != 'level':
+            # if its not an int like 'name' then do nothing till next elif.
+                # Then we're going to increment that value by our stat increase which is 3x increment.
                 dict_items[k] += stat_increase
+            # Otherwise if its 'level' then we only raise that by 1x the increment.
             elif k == 'level':
                 dict_items[k] += increment
+            # 'name' doesnt get changed.
 
         self.__dict__.update(dict_items)
 
